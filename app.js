dd1aff815c5ff97f54207b56abb53c6643239461
Julian Lindsay-Kaufman <jjjuliannnn456@gmail.com>
2022-04-15 10:36:14 -0400

    radial twirl effect and trail fade-out

--- app.js
+++ app.js
@@ -1,48 +1,105 @@
 const canvas = document.getElementById('canvas1');
-const ctx = canvas.getContext('2d');
+const c = canvas.getContext('2d');
 
-ctx.canvas.width = window.innerWidth;
-ctx.canvas.height = window.innerHeight;
-let particleArray;
+c.canvas.width = window.innerWidth;
+c.canvas.height = window.innerHeight;
+
+
+
+const baseSpeed = 0.5;
+// const baseForce = 0.1;
+// const maxDistance = 1000;
+// const gravitation = 3;
+// const friction = 0.9;
+
+
+
+// mouse position
+let mouse = {
+    x: null,
+    y: null,
+    radius: 100
+};
+
+
+
+window.addEventListener('mousemove',
+    function(event) {
+        mouse.x = event.x;
+        mouse.y = event.y;
+    }
+)
 
 
 
 // Particle constructor
-function Particle(x, y, directionX, directionY, size, color) {
+function Particle(x, y, radius, color) {
     this.x = x;
     this.y = y;
-    this.directionX = directionX;
-    this.directionY = directionY;
-    this.size = size;
+    // this.directionX = directionX;
+    // this.directionY = directionY;
+    // this.size = size;
+    this.radius = radius;
     this.color = color;
+    this.radians = Math.random() * Math.PI * 2;
+    this.velocity = 0.05;
 }
 
 
 
 // Draw particles
 Particle.prototype.draw = function() {
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-    ctx.fillStyle = this.color;
-    ctx.fill();
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    c.fillStyle = this.color;
+    c.fill();
+    c.closePath()
 }
 
 
 
 // Update particles
 Particle.prototype.update = function() {
-    if (this.x + this.size >= canvas.width || this.x - this.size <= 0) {
-        this.directionX *= -1;
-    }
+    
+    // wall collision
+    // if (this.x + this.size >= canvas.width || this.x - this.size <= 0) {
+    //     this.directionX *= -(1-friction);
+    // }
 
-    if (this.y + this.size >= canvas.height || this.y - this.size <= 0) {
-        this.directionY *= -1;
-    }
+    // if (this.y + this.size >= canvas.height || this.y - this.size <= 0) {
+    //     this.directionY *= -(1-friction);
+    // }
 
-    this.x += this.directionX;
-    this.y += this.directionY;
+
+
+    // circular motion
+    this.radians += this.velocity
+    this.x = this.x + Math.cos(this.radians) * 10
+    this.y = this.y + Math.sin(this.radians) * 10
+   
+
+
+    // attraction to mouse
+    // let dx = mouse.x - this.x;  
+    // let dy = mouse.y - this.y;
+    // let distance = Math.sqrt(dx * dx + dy * dy);
+    // let forceDirectionX = dx / distance;
+    // let forceDirectionY = dy / distance;
+    
+    // let force = (maxDistance - distance) / maxDistance;
+    // if (force < 0) force = 0;
+    
+    // // this.directionX = force * forceDirectionX * gravitation;
+    // // this.directionY = force * forceDirectionY * gravitation;
+
+
+
+
+    // this.x += this.directionX;
+    // this.y += this.directionY;
 
     this.draw();
+    // console.log(this.radians, this.x, this.y)
 }
 
 
@@ -58,14 +115,14 @@ function init() {
             blue: Math.random() * 255,
         }
 
-        let size = Math.random() * 20;
-        let x = Math.random() * (innerWidth - size * 2) + size;
-        let y = Math.random() * (innerHeight - size * 2) + size;
-        let directionX = (Math.random() * 0.4) - 0.2;
-        let directionY = (Math.random() * 0.4) - 0.2;
+        // let size = Math.random() * 20;
+        // let x = Math.random() * (innerWidth - (size * 2)) + size;
+        // let y = Math.random() * (innerHeight - (size * 2)) + size;
+        // let directionX = (Math.random() * baseSpeed * 2) - baseSpeed;
+        // let directionY = (Math.random() * baseSpeed * 2) - baseSpeed;
         let color = `rgb(${colors.red}, ${colors.green}, ${colors.blue})`;
 
-        particleArray.push(new Particle(x, y, directionX, directionY, size, color));
+        particleArray.push(new Particle(canvas.width / 2, (canvas.height / 2), 10, color));
     }
 }
 
@@ -74,11 +131,13 @@ function init() {
 // Animation
 function animate() {
     requestAnimationFrame(animate);
-    ctx.clearRect(0, 0, innerWidth, innerHeight);
+    // ctx.clearRect(0, 0, innerWidth, innerHeight);
+    c.fillStyle = 'rgba(255,255,255,0.05';
+    c.fillRect(0,0,canvas.width,canvas.height);
 
-    for (let i = 0; i < particleArray.length; i++) {
-        particleArray[i].update();
-    }
+    particleArray.forEach(particle => {
+        particle.update()
+    })
 }
 
 
